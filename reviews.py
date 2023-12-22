eec57371cef6e0470219b3a770e4eca1a209237b
gainfal <gainfal@gmail.com>
2023-12-21 21:57:42 -0600

    w r

--- reviews.py
+++ reviews.py
@@ -1,22 +1,21 @@
 import pandas as pd
 
-# Read the CSV file
-file_path = 'CLCK-Data-Aug2023/wine-reviews-exercise-gainfal/data/winemag-data-130k-v2.csv.zip'
-df = pd.read_csv(file_path, compression='zip')
+def create_summary(input_file_path, output_file_path):
+    # Read the CSV file
+    df = pd.read_csv(input_file_path, compression='zip')
 
-# Group data by country and calculate count and mean
-summary_data = df.groupby('country').agg({'points': ['count', 'mean']}).reset_index()
+    # Group data by country and calculate count and mean
+    summary_data = df.groupby('country').agg({'points': ['count', 'mean']}).reset_index()
 
-# Rename columns for better clarity
-summary_data.columns = ['country', 'count', 'points']
+    # Rename columns for better clarity
+    summary_data.columns = ['country', 'count', 'points']
 
-# Sort the data by count in descending order
-summary_data = summary_data.sort_values(by='count', ascending=False)
+    # Sort the data by count in descending order
+    summary_data = summary_data.sort_values(by='count', ascending=False)
 
-# Write the summary data to a new CSV file
-output_file_path = 'data/reviews-per-country.csv'
-summary_data.to_csv(output_file_path, index=False)
+    # Write the summary data to a new CSV file
+    summary_data.to_csv(output_file_path, index=False)
 
-print(f"Summary data written to {output_file_path}")
+    print(f"Summary data written to {output_file_path}")
 
 
